a339ea31b595a7e9ff6b531d3f8653e5fc788720
simeee <whdk2340@gmail.com>
2023-07-13 11:57:14 +0900

    obj, function 실습

--- js/index.js
+++ js/index.js
@@ -14,7 +14,11 @@ if (hour < 12) {
   alert("오후");
 }
 
-let input = Number(prompt("숫자입력", "숫자"));
+let input = prompt("숫자입력", "숫자");
+console.log(typeof input); //문자열
+input = Number(input); //문자열을 숫자형태로 감싸줘야 switch문 에러안남
+console.log(typeof input);
+
 switch (input % 2) {
   case 0:
     console.log("짝수");
@@ -26,3 +30,37 @@ switch (input % 2) {
     console.log("숫자가 아닙니다.");
     break;
 }
+
+//배열
+let dayOfWeek = ["Monday", 54, true, [1, 2, 4]];
+
+console.log(dayOfWeek[3]);
+
+//객체
+let obj = {
+  name: "JS",
+  favoriteFoods: [
+    {
+      name: "banana",
+      isHealthy: true,
+    },
+    {
+      name: "pizza",
+      isHealthy: false,
+    }, //obj안에 배열 안에 obj
+    [1, 2, 3],
+    1212,
+  ],
+};
+
+console.log(obj.name);
+console.log(obj.favoriteFoods[1].isHealthy);
+console.log(obj.favoriteFoods[2][2]);
+
+const person = {
+  greet: function () {
+    return "hello";
+  },
+};
+
+console.log(person.greet());
