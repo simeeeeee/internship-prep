7cc811276e0ba3895bdf2647508c221da1ff5029
simeee <whdk2340@gmail.com>
2023-07-13 15:48:43 +0900

    fizzbuzz

---
+++ js/practice2.js
@@ -0,0 +1,36 @@
+function multiplyTable(number) {
+  for (let i = 1; i < 10; i++) {
+    console.log(`${number} *${i} = ${number * i}`);
+  }
+}
+let input = Number(prompt("숫자를 입력하시오"));
+multiplyTable(input);
+
+console.log(true || false);
+console.log(true && true);
+console.log(true || false);
+console.log(!false);
+
+isEligible = true;
+if (!isEligible) {
+  console.log("에러 메세지");
+}
+
+//1부터 30까지 춧자를 돌면서
+//3배수면 'fizz'
+//5     'buzz'
+//3,5배수면 'fizzbuzz'
+//나머진 그냥출력
+let i = 1;
+while (i <= 30) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 == 0) {
+    console.log("fizz");
+  } else if (i % 5 == 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+  i++;
+}
